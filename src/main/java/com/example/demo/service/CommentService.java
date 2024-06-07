fd9bb09709d025142eaa84e94121d41f468795df
Arklimits <Arklimits@users.noreply.github.com>
2024-06-08 06:38:14 +0900

    CommentService 기본 기능 구성

--- src/main/java/com/example/demo/service/CommentService.java
+++ src/main/java/com/example/demo/service/CommentService.java
@@ -15,17 +15,35 @@ public class CommentService {
     private CommentRepository commentRepository;
     private PostRepository postRepository;
 
+    /**
+     * Comment 저장
+     *
+     * @param postId  Post ID
+     * @param comment Comment
+     * @return 저장된 Comment
+     */
     public Comment saveComment(Long postId, Comment comment) {
-        Post post = postRepository.findById(postId).orElseThrow(()->new RuntimeException("Post not found"));
+        Post post = postRepository.findById(postId).orElseThrow(() -> new RuntimeException("Post not found"));
         comment.setPost(post);
         return commentRepository.save(comment);
     }
 
+    /**
+     * PostId 에 맞는 Comment 찾기
+     *
+     * @param postId Post ID
+     * @return 생성날짜 기준으로 내림차순된 Comments
+     */
     public List<Comment> getCommentsByPostId(Long postId) {
         return commentRepository.findByPostIdCommentOrderedByDateDesc(postId);
     }
 
-    public void deleteComment(Long id){
+    /**
+     * Comment 삭제
+     *
+     * @param id
+     */
+    public void deleteComment(Long id) {
         commentRepository.deleteById(id);
     }
 }
