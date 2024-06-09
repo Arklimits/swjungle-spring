fa225b8606c22a75b538c00bc3c47d94112d9fc6
Arklimits <Arklimits@users.noreply.github.com>
2024-06-09 13:18:23 +0900

    Comment 수정기능 추가

--- src/main/java/com/example/demo/service/CommentService.java
+++ src/main/java/com/example/demo/service/CommentService.java
@@ -2,8 +2,14 @@ package com.example.demo.service;
 
 import com.example.demo.controller.dto.CommentRequestDTO;
 import com.example.demo.controller.dto.CommentResponseDTO;
+import com.example.demo.controller.dto.PostRequestDTO;
+import com.example.demo.controller.dto.PostResponseDTO;
 import com.example.demo.model.Comment;
+import com.example.demo.model.Post;
 import com.example.demo.repository.CommentRepository;
+import com.example.demo.repository.PostRepository;
+import com.example.demo.repository.UserRepository;
+import jakarta.validation.constraints.Null;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +19,8 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class CommentService {
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
     private CommentRepository commentRepository;
 
     /**
@@ -24,7 +32,24 @@ public class CommentService {
      */
     public CommentResponseDTO saveComment(Long postId, CommentRequestDTO commentRequestDTO) {
         String date = java.time.LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        if (postRepository.findById(postId).isEmpty()) {
+            throw new NullPointerException("Post does not exist");
+        }
         Comment comment = new Comment(commentRequestDTO.getContent(), UserService.getCurrentUsername(), date, postId);
+
+        return new CommentResponseDTO(commentRepository.save(comment));
+    }
+
+    /** Comment 업데이트
+     *
+     * @param id Comment ID
+     * @param commentRequestDTO Original CommentResponseDTO
+     * @return Updated CommentResponseDTO
+     */
+    public CommentResponseDTO updateComment(long id, CommentRequestDTO commentRequestDTO) {
+        String date = java.time.LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        Comment comment = commentRepository.findById(id).orElseThrow(()-> new NullPointerException("Comment not found"));
+        comment.editComment(commentRequestDTO.getContent(), date);
 
         return new CommentResponseDTO(commentRepository.save(comment));
     }
