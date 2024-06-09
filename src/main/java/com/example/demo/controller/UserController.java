d8c1e8512609acc55802096f51845f367f67f2e9
Arklimits <Arklimits@users.noreply.github.com>
2024-06-09 15:29:44 +0900

    Register 반환 타입 String -> ResponseEntity

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,6 +4,8 @@ import com.example.demo.controller.dto.user.UserRequestDTO;
 import com.example.demo.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +16,13 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public String registerUser(@Valid @RequestBody UserRequestDTO userRequestDTO) {
+    public ResponseEntity<String> registerUser(@Valid @RequestBody UserRequestDTO userRequestDTO) {
         if (userService.findByUsername(userRequestDTO.getUsername()) != null) {
-            return "exist username";
+
+            return new ResponseEntity<>("exist username", HttpStatus.NOT_ACCEPTABLE);
         }
 
         userService.saveUser(userRequestDTO);
-        return "register complete";
+        return new ResponseEntity<>("exist username", HttpStatus.OK);
     }
 }
